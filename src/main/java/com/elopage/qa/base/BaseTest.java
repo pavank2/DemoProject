3ce6c47584219e94a3d18c6c04991d63868c75f8
Pavan <pavankumar.kulkarni@softwareag.com>
2021-11-19 12:15:46 +0530

    Support for Grid

--- src/main/java/com/elopage/qa/base/BaseTest.java
+++ src/main/java/com/elopage/qa/base/BaseTest.java
@@ -3,9 +3,14 @@ package com.elopage.qa.base;
 import com.elopage.qa.factory.DriverFactory;
 import com.elopage.qa.util.ConfigReader;
 import com.elopage.qa.util.TestUtil;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -16,14 +21,32 @@ public class BaseTest {
     private ConfigReader configReader;
 
     public WebDriver initialize() {
-        configReader = new ConfigReader();
-        prop = configReader.init_prop();
-        String browserName = prop.getProperty("browser");
-        driverFactory = new DriverFactory();
+//        configReader = new ConfigReader();
+//        prop = configReader.init_prop();
+//        String browserName = prop.getProperty("browser");
+//        driverFactory = new DriverFactory();
+//        driver = driverFactory.init_driver(browserName);
 
-      //  System.setProperty("webdriver.chrome.driver", "C://Users//PK//Downloads//chromedriver.exe");
-        driver = driverFactory.init_driver(browserName);
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--enable-automation");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--dns-prefetch-disable");
+        options.addArguments("--disable-gpu");
+       // options.addArguments("--headless");
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        //    Docker config:  "http://10.60.28.135:4444"
+        //K8S config: "http://52.66.70.68:32237/wd/hub"
+        try {
+            String url = System.getProperty("url");
+
+            driver = new RemoteWebDriver(new URL(url),options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
