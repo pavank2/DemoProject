99649c3499cc2e2e7f2276b58ac34f0ba98c9c7c
Pavan <pavankumar.kulkarni@softwareag.com>
2021-11-19 13:35:51 +0530

    Options

--- src/main/java/com/elopage/qa/base/BaseTest.java
+++ src/main/java/com/elopage/qa/base/BaseTest.java
@@ -3,10 +3,12 @@ package com.elopage.qa.base;
 import com.elopage.qa.factory.DriverFactory;
 import com.elopage.qa.util.ConfigReader;
 import com.elopage.qa.util.TestUtil;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -26,26 +28,48 @@ public class BaseTest {
 //        String browserName = prop.getProperty("browser");
 //        driverFactory = new DriverFactory();
 //        driver = driverFactory.init_driver(browserName);
+        String browser = System.getProperty("browser");
+        if (browser == "chrome") {
+          ChromeOptions  options = new ChromeOptions();
+            options.addArguments("--enable-automation");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--dns-prefetch-disable");
+            options.addArguments("--disable-gpu");
+            // options.addArguments("--headless");
+            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+            try {
+                String url = System.getProperty("url");
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--enable-automation");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--dns-prefetch-disable");
-        options.addArguments("--disable-gpu");
-       // options.addArguments("--headless");
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                driver = new RemoteWebDriver(new URL(url),options);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--enable-automation");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--dns-prefetch-disable");
+            options.addArguments("--disable-gpu");
+            // options.addArguments("--headless");
+            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+            try {
+                String url = System.getProperty("url");
+
+                driver = new RemoteWebDriver(new URL(url),options);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
 
         //    Docker config:  "http://10.60.28.135:4444"
         //K8S config: "http://52.66.70.68:32237/wd/hub"
-        try {
-            String url = System.getProperty("url");
 
-            driver = new RemoteWebDriver(new URL(url),options);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
